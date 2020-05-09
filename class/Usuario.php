06037dd215bdeaf0773a2849f6c5a846b36800bc
Robinho de Morais <robinhodemorais@gmail.com>
2020-05-08 21:53:54 -0300

    metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -55,6 +55,43 @@ class Usuario {
         }
     }
 
+    //quando não tem uso da classe this, podemos usar o metodo como
+    //estatico, dessa forma pode chamar o metodo direto, por não instaciar
+    public static function getList(){
+        $sql = new Sql();
+        
+        return $sql->select("select * from tb_usuarios order by deslogin");
+    }
+
+    public static function search($login){
+        $sql = new Sql();
+       
+        return $sql->select("select * from tb_usuarios where deslogin like :SEARCH order by deslogin", array(
+            ':SEARCH'=>"%".$login."%"
+        ));   
+    }
+
+    public function login($login,$password){
+        $sql = new Sql();
+       
+        $results = $sql->select ("select * from tb_usuarios where deslogin = :LOGIN and dessenha =:PASSWORD",array(
+            ":LOGIN"=>$login,
+            ":PASSWORD"=>$password
+        ));
+
+        if (count($results) > 0) {
+            $row = $results[0];
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+        } else {
+            throw new Exception("Login e/ou senha inválidos.");
+        } 
+    }
+
+
     public function __toString()
     {
         return json_encode((array(
--- index.php
+++ index.php
@@ -8,10 +8,25 @@ $usuarios = $sql->select("select * from tb_usuarios");
 echo json_encode($usuarios);
 */
 
-$root = new Usuario();
 
-$root->loadById(1);
+//retona 1 usuario com o id
+//$root = new Usuario();
+//$root->loadById(1);
+//echo $root;
 
-echo $root;
+//carrega lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//carrega uma lista pelo login
+//$search = Usuario::search("ro");
+//echo json_encode($search);
+
+//carrega o usuario
+$usuario = new Usuario();
+
+$usuario->login("root","123");
+
+echo $usuario;
 
 ?>
